8bfa8b1fc1fbd1011b768505f1ca46d3e5f7d117
akshayr1298 <akshayrpanicker005@gmail.com>
2024-02-03 17:58:48 +0530

    add hover chnges

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,41 @@
 import React from "react";
 import headerLogo from "../../assets/instagram.png";
 
-const Header = () => {
 
-  const reload = ()=>{
+const Header = () => {
+  const reload = () => {
     window.location.reload();
-  }
+  };
 
   return (
     <>
-      <div className="bg-neutral-800 w-full min-h-14 flex justify-around">
-        <div className="w-full flex justify-around">
-          <button className="flex items-center justify-center px-4  bg-neutral-800 text-white rounded-md" onClick={reload}>
+      <div className="bg-neutral-800 w-full min-h-14 flex justify-around grid-cols-3">
+        <div className="w-full flex">
+          <button
+            className="flex items-center justify-center px-4  bg-neutral-800 text-white rounded-md mx-9"
+            onClick={reload}
+          >
             <img src={headerLogo} alt="Logo" className="h-8 w-8 mr-2" />
           </button>
+          <p className="text-white font-bold mt-4 cursor-pointer transition-colors duration-300 hover:text-green-500">
+            AMIGO
+          </p>
         </div>
-        <div  className="w-full flex justify-around">
-          <p className="text-white font-semibold mt-3 cursor-pointer">Home</p>
-          <p className="text-white font-semibold mt-3 cursor-pointer">About us</p>
-          <p className="text-white font-semibold mt-3 cursor-pointer">Contact</p>
+        <div className="w-full flex justify-around">
+          <p className="text-white font-semibold mt-3 cursor-pointer transition-colors duration-300 cursor-pointer hover:text-green-500">
+            Home
+          </p>
+          <p className="text-white font-semibold mt-3 cursor-pointer transition-colors duration-300 cursor-pointer hover:text-green-500">
+            About us
+          </p>
+          <p className="text-white font-semibold mt-3 cursor-pointer transition-colors duration-300 cursor-pointer hover:text-green-500">
+            Contact
+          </p>
         </div>
-        <div className="w-full flex justify-around ">
-        <p className="text-white font-semibold mt-3 cursor-pointer">right</p>
+        <div className="w-full flex justify-around">
+          <p className="text-white font-semibold mt-3 cursor-pointer transition-colors duration-300 cursor-pointer hover:text-green-500">
+            Profile
+          </p>
         </div>
       </div>
     </>

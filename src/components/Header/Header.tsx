d8f62c0d57e0242188bae04bb8819bb180a9db66
akshayr1298 <akshayrpanicker005@gmail.com>
2024-02-03 17:00:59 +0530

    add header logo and home,about,contact tag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import headerLogo from "../../assets/header.png"
+import React from "react";
+import headerLogo from "../../assets/instagram.png";
 
 const Header = () => {
+
+  const reload = ()=>{
+    window.location.reload();
+  }
+
   return (
     <>
-    <div className='bg-neutral-600 w-full min-h-14 flex justify-around'>
-        <div>
-    <button className="flex items-center justify-center px-4 py-2 bg-blue-500 text-white rounded-md mt-2">
-    <img src={headerLogo} alt="Logo" className="h-6 w-6 mr-2"/>
-  </button>
+      <div className="bg-neutral-800 w-full min-h-14 flex justify-around">
+        <div className="w-full flex justify-around">
+          <button className="flex items-center justify-center px-4  bg-neutral-800 text-white rounded-md" onClick={reload}>
+            <img src={headerLogo} alt="Logo" className="h-8 w-8 mr-2" />
+          </button>
         </div>
-        <div>
-            login header
+        <div  className="w-full flex justify-around">
+          <p className="text-white font-semibold mt-3 cursor-pointer">Home</p>
+          <p className="text-white font-semibold mt-3 cursor-pointer">About us</p>
+          <p className="text-white font-semibold mt-3 cursor-pointer">Contact</p>
         </div>
-
-    </div>
+        <div className="w-full flex justify-around ">
+        <p className="text-white font-semibold mt-3 cursor-pointer">right</p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
